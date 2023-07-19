9e26a62f099e31aee0ae6d094f70474e6aa294d2
Lucas Brest <lucasezequiel237@gmail.com>
2023-07-19 11:02:33 -0300

    Add responsive productPage

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Paper, Stack, Typography } from "@mui/material"
+import { Box, Button, CardMedia, Paper, Stack, Typography } from "@mui/material"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import productService from "../services/productService"
@@ -37,52 +37,58 @@ const ProductPage = () => {
     { product && 
     <>
       <Stack spacing={2} sx={{
-        display:"flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "space-between",
-        alignItems:"flex-start",
         backgroundColor: "white",
         padding: 4,    
       }}>
-        <Box>
-          <img 
-            src={product.image} 
-            alt={product.title} 
-            style={{
-              maxWidth:'450px', 
-              maxHeight:'450px'
-            }}/>
-        </Box>
-        <Paper elevation={12} sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          width:'450px',
-          padding: 2,
-        }}>
-          <Typography variant="h6" color="textSecondary" component="p">
-            {product.category}
-          </Typography>
-          <Typography gutterBottom variant="h5" component="h2" marginY={2}>
-            {product.title}
-          </Typography>
-          <RatingStars rating={product.rating}/>
-          <Typography variant="h3" component="p" marginY={2}>
-            $ {product.price}
-          </Typography>
-          {
-            isConnected && <>
-              <Stack flexDirection='row' alignItems='center' marginY={2}>
-                <Button variant="outlined" onClick={() => setQty(qty - 1)} disabled={qty < 1}><Remove/></Button>
-                <Typography variant="h6" paddingX={2} sx={{userSelect:'none'}}>{qty}</Typography>
-                <Button variant="outlined" onClick={() => setQty(qty + 1)} disabled={qty > 9}><Add/></Button>
-              </Stack>
-              <Button variant="contained" disabled={qty < 1} onClick={handleAddToCart}>
-                Add to Cart
-              </Button>
-            </>
-          }
-        </Paper>
+        <Stack flexDirection={{xs:'column', md:'row'}} justifyContent='space-between' width='100%'>
+          <Paper elevation={12} sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            width:{xs:'100%', md:'45%'},
+            height:'100%',
+            padding: 2,
+          }}>
+            <CardMedia 
+              component='img'
+              image={product.image}
+              alt={product.title} 
+              style={{
+                maxHeight:'450px',
+                objectFit: 'contain'
+              }}
+              />
+          </Paper>
+          <Paper elevation={12} sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            width:{xs:'100%', md:'45%'},
+            height:'100%',
+            padding: 2,
+          }}>
+            <Typography variant="h6" color="textSecondary" component="p">
+              {product.category}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="h2" marginY={2}>
+              {product.title}
+            </Typography>
+            <RatingStars rating={product.rating}/>
+            <Typography variant="h3" component="p" marginY={2}>
+              $ {product.price}
+            </Typography>
+            {
+              isConnected && <>
+                <Stack flexDirection='row' alignItems='center' marginY={2}>
+                  <Button variant="outlined" onClick={() => setQty(qty - 1)} disabled={qty < 1}><Remove/></Button>
+                  <Typography variant="h6" paddingX={2} sx={{userSelect:'none'}}>{qty}</Typography>
+                  <Button variant="outlined" onClick={() => setQty(qty + 1)} disabled={qty > 9}><Add/></Button>
+                </Stack>
+                <Button variant="contained" disabled={qty < 1} onClick={handleAddToCart}>
+                  Add to Cart
+                </Button>
+              </>
+            }
+          </Paper>
+        </Stack>
         <Typography variant="h6" component="p" paddingY={2}>
           {product.description}
         </Typography>
